85f1e8748f6f086824aa19395f76fae8c984fd12
Alexander Zorkin <somnoynadno@yandex.ru>
2021-06-05 10:35:24 +0700

    Necessary dependencies installed

--- services/Frontend/WebPanel/src/App.js
+++ services/Frontend/WebPanel/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello
+        </div>
+    );
 }
 
 export default App;
