dd6bdff5d6b237cc414a3abb4f1e36cc9b0e6865
amoniaka-knabino <sv@dynfor.ru>
2021-06-06 10:42:40 +0500

    tests fix

--- services/Backend/EnumAPI/test_enums.py
+++ services/Backend/EnumAPI/test_enums.py
@@ -4,7 +4,6 @@ import json
 
 class TestEnumAPI(unittest.TestCase):
     def test_account_types(self):
-        enc = requests.get("http://enum.dev.somnoynadno.ru/api/bank_account_types/1").json()
-        r = json.loads(enc)
+        r = requests.get("http://enum.dev.somnoynadno.ru/api/bank_account_types/1").json()
         self.assertEqual(r["ID"],1)
         self.assertEqual(r["Name"],"DepositAccount")
